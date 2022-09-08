d1737644a036a0f42a98a55239a141f0e1e9d979
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2022-09-08 04:24:02 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ use rustls_pemfile::{certs, pkcs8_private_keys};
 use actix_web::{middleware, App, HttpServer, get, Responder};
 use actix_files::NamedFile;
 use actix_web_lab::{header::StrictTransportSecurity, middleware::RedirectHttps};
-
+use uuid::Uuid;
 use chrono::prelude::*;
 
 #[get("/")]
-async fn index() -> impl Responder {
+async fn index(req: HttpRequest) -> impl Responder {
+    let txid = Uuid::new_v4();
+    let peer = req.peer_addr();
+    // To log the headers, bring in requ and add that to the info log, like at the end.
+    //let requ = req.headers();
+    log::info!("{} Transaction ID generated for {:?} visiting website root", txid, peer);
     NamedFile::open_async("./static/index.html").await
 }
 
@@ -23,12 +28,21 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(RedirectHttps::default())
             .wrap(RedirectHttps::with_hsts(StrictTransportSecurity::recommended()))
+            // Comment this out since it is handled by the above RedirectHttps to set the header
+            // instead.
             //.wrap(middleware::DefaultHeaders::new().add(("strict-transport-security", "max-age=31536000; includeSubdomains;")))
+            // Note: Expect-CT header not used by default. Add as required, following the same
+            // style below.
             .wrap(middleware::DefaultHeaders::new().add(("x-content-type-options", "nosniff")))
             .wrap(middleware::DefaultHeaders::new().add(("x-frame-options", "SAMEORIGIN")))
             .wrap(middleware::DefaultHeaders::new().add(("x-xss-protection", "1; mode=block")))
-            .wrap(middleware::Logger::default())
+            // this access logging can be optionally commented to only log Transaction function
+            // data above :)
+            //.wrap(middleware::Logger::default())
+            // We'll bring in just a more minimal format by default for the middleware logger:
+            .wrap(middleware::Logger::new("%a -> HTTP %s %r size: %b time: %T"))
             .service(index)
+            // add additional services here after index service before the static Files service below
             .service(Files::new("/", "static"))
 
     })
@@ -55,7 +69,7 @@ fn load_rustls_config() -> rustls::ServerConfig {
         .collect();
     if keys.is_empty() {
         let readu: DateTime<Utc> = Utc::now();
-        eprintln!("{} - morpho FATAL - Open of /app/privkey.pem paired with /app/cert.pem failed, server must shutdown.", readu);
+        eprintln!("{} - morpho FATAL - Open of privkey.pem paired with cert.pem failed, server must shutdown. Use PKCS8 PEM", readu);
         std::process::exit(1);
     }
     config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
